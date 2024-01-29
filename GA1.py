5af00fb6dd8b98616bfab04bb1b56c3d5da54343
Joseph Di Lullo <85251118+joed123@users.noreply.github.com>
2024-01-29 12:12:55 -0800

    Update GA1.py

--- GA1.py
+++ GA1.py
@@ -1,46 +1,86 @@
-'''
-This file contains the template for Assignment1. For testing it, I will place
-it
-in a different directory, call the function <number_of_allowable_intervals>,
-and check
-its output. So, you can add/remove whatever you want to/from this file. But,
-don't
-change the name of the file or the name/signature of the following function.
-Also, I will use <python3> to run this code.
 
-def number_of_allowable_intervals(input_file_path, output_file_path):
-    
-    This function will contain your code. It wil read from the file
-    <input_file_path>,
-    and will write its output to the file <output_file_path>.
-    
-    pass '''
-
-
-
-def number_of_allowable_intervals(input_file_path, output_file_path):
-    '''
-    Read the input file containing intervals and calculate the number of allowable intervals.
-
-    Parameters:
-        input_file_path (str): Path to the input file containing intervals.
-        output_file_path (str): Path to the output file to write the result.
-
-    Returns:
-        None
-    '''
-    # Read input file
-    with open(input_file_path, 'r') as input_file:
-        intervals = input_file.readlines()
-
-    #our input consists of (i) two arrays A[1, . . . , m] and B[1, . . . , n] of integers
-    # (ii) a target value range denoted by [tmin, tmax].
-
-
-
-    # Write result to output file
-    with open(output_file_path, 'w') as output_file:
-        output_file.write(str(num_allowable_intervals))
 
 # Example usage:
 # number_of_allowable_intervals('input.txt', 'output.txt')
+        
+
+
+# Returns the amount of numbers between the min and max values
+def find_values_between(arr, min_val, max_val):
+    # Check if max is less than all values in array
+    if max_val < arr[0]:
+        return 0
+
+    # Check if min is greater than all values in array
+    elif min_val > arr[len(arr) - 1]:
+        return 0
+
+    min_idx = find_closest(arr, min_val)
+    # Check if we are including a value below the min value
+    if arr[min_idx] < min_val:
+        min_idx += 1
+
+    max_idx = find_closest(arr, max_val)
+
+    # return the number of values between max and min
+    return max_idx - min_idx + 1
+
+# Returns element index of target in arr[] or index of the value below where target would be
+# Runs in O(lg(n)) time
+def find_closest(arr, target):
+
+    n = len(arr)
+    if target <= arr[0]:
+        return 0
+    if target >= arr[n - 1]:
+        return n - 1
+
+    # Doing binary search to find closest value lg(n) time
+    i = 0
+    j = n
+    mid = 0
+    while i < j:
+        mid = (i + j) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        if target < arr[mid]:
+
+            # If target is greater than previous
+            # return previous index
+            if mid > 0 and target > arr[mid - 1]:
+                return mid - 1
+
+            # Repeat for other half
+            j = mid
+
+        # If target is greater than mid
+        else:
+            if mid < n - 1 and target < arr[mid + 1]:
+                return mid
+
+            # update i
+            i = mid + 1
+
+    return mid
+
+
+# Finds possible pairs in O(nlg(n) + mlg(n)) time
+def possible_pairs(A, B, t_min, t_max):
+
+    # Sorts A in nlg(n) time
+    A.sort()
+
+    total = 0
+    # Loops through B and finds pairs in mlg(n) time
+    for x in B:
+        temp_min = t_min - x
+        temp_max = t_max - x
+        # Finds values between min and max in lg(n) time
+        total += find_values_between(A, temp_min, temp_max)
+
+    return total
+
+
+print(possible_pairs([0, 2, 1, 0, 0, 5, 17, 0], [-3, 10, 0], -3, 20))
